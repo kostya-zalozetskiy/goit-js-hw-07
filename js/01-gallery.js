1bb4d720760c680bbbffc87014cf4f87c04c550b
kostya-zalozetskiy <kostya_zalozetskiy@outlook.com>
2022-01-07 19:45:57 +0200

    sadd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const createImageList = document.querySelector('.gallary');
 
-console.log(galleryItems);
+// const createGalleryMurkupList = createGalleryMurkup(galleryItems);
+console.log(createGalleryMurkup);
+
+const createGalleryMurkup = galleryItems.map(({ original, preview, description }) => {
+        return
+        `
+        <div class="gallery__item">
+         <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+           />
+         </a>
+         </div>
+        `;
+    }).join('')
+
+
+
+//  <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+
+// console.log(galleryItems);
